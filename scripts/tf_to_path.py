990d0be5b3680911110c0387cf362b0bdf6e987a
engcang <engcang@naver.com>
2020-09-26 00:54:50 +0900

    unnecessary part

--- scripts/tf_to_path.py
+++ scripts/tf_to_path.py
@@ -44,15 +44,6 @@ class path_pub():
                 self.pose = msg.transforms[i].transform
                 self.check = 1
 
-    def my_callback(self, msg):
-        self.check_my = 1
-        my_p = msg
-        my_p.header.frame_id = 'map'
-        my_p.header.stamp = rospy.Time.now()
-        self.my_path.header.frame_id='map'
-        self.my_path.poses.append(my_p)
-        self.my_path.header.stamp = rospy.Time.now()
-
 ''' main '''
 path_pub_class = path_pub()
 
@@ -68,9 +59,9 @@ if __name__ == '__main__':
                 pose.pose.orientation.y = path_pub_class.pose.rotation.y
                 pose.pose.orientation.z = path_pub_class.pose.rotation.z
                 pose.pose.orientation.w = path_pub_class.pose.rotation.w
-                pose.header.frame_id = path_pub_class.parent_frame_id
+                pose.header.frame_id = "/map"
                 pose.header.stamp = rospy.Time.now()
-                path_pub_class.my_path.header.frame_id=path_pub_class.parent_frame_id
+                path_pub_class.my_path.header.frame_id="/map"
                 path_pub_class.my_path.poses.append(pose)
                 path_pub_class.my_path.header.stamp = rospy.Time.now()
                 path_pub_class.my_path_pub.publish(path_pub_class.my_path)
